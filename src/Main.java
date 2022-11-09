99eabde37ad98a3440aad095083b346efce2f2f5
DiabloKG <303mem@gmail.com>
2022-11-09 13:54:30 +0600

    Сделал все на массивах без листов

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,38 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
-    private static void swap(double[] array, int ind1, int ind2) {
-        double tmp = array[ind1];
-        array[ind1] = array[ind2];
-        array[ind2] = tmp;}
-
     public static void main(String[] args) {
-        List<Double> pstvnums = new ArrayList();
-        List ngtvlist = new ArrayList();
-        boolean ngtvnums = false;
-
+        double sum = 0.0;
+        double num = 0;
+        double ngtv = 0.0;
         double[] nums1 = {12.6, 254.21, -45.2, 69.5, -144.21, 78.36, -34.26, 64.12, -788.365, 546.21, 25.456, -785.21,
                 126.123, 723.4, 1.25};
         for (double nums2 : nums1) {
-            if (ngtvnums == false && nums2 > 0){
+            if (nums2 > 0 && ngtv == 0) {
                 continue;
-            } if (ngtvnums == true && nums2 > 0){
-                pstvnums.add(nums2);
-            } if (ngtvnums == false && nums2 < 0){
-                ngtvnums = true;
-                ngtvlist.add(nums2);
             }
+            if (nums2 < 0) {
+                ngtv++;
             }
-        double average = 0.0;
-        double sum = 0;
-        for (double num: pstvnums) {
-            sum += num;
+            if (nums2 > 0) {
+                num++;
+                sum = sum + nums2;
+            }
         }
-        average = sum/pstvnums.size();
-        System.out.println("\n1. Среднее число = " + average);
-
-
-        for (int left = 0; left < nums1.length; left++) {
-            int minInd = left;
-            for (int i = left; i < nums1.length; i++) {
-                if (nums1[i] < nums1[minInd]) {
-                    minInd = i;
+        System.out.println("Среднее число: " + sum / num);
+        for (int i = 0; i < nums1.length; i++){
+            int min = i;
+            for (int j = i + 1; j < nums1.length; j++){
+                if (nums1[j] < nums1[min]) {
+                    min = j;
                 }
             }
-            swap(nums1, left, minInd);
+            if (min != i){
+                double temp = nums1[i];
+                nums1[i] = nums1[min];
+                nums1[min] = temp;
+            }
+            System.out.print(nums1[i] + ", " );
         }
-        System.out.println("\n2. От меньшего к большему = " + Arrays.toString(nums1));
     }
-    }
+}
+
+
